6289b6c4391a71974f911e6ec6471e0f2b534637
aritrashome <43993552+aritrashome@users.noreply.github.com>
2021-08-27 18:14:12 +0530

    Add files via upload

---
+++ scripts/granger_causality.py
@@ -0,0 +1,38 @@
+#!pip install spectral_connectivity
+
+from statsmodels.tsa.stattools import grangercausalitytests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+from tqdm import tqdm
+
+from spectral_connectivity import Multitaper, Connectivity
+def granger_cause(x,y):
+    m = Multitaper(time_series=np.array([x,y]).transpose(),sampling_frequency=250, n_fft_samples=125)
+    c = Connectivity.from_multitaper(m)
+    matrix = c.pairwise_spectral_granger_prediction()
+    matrix = np.nan_to_num(matrix, nan=0)
+    return matrix[0,:,0,1].mean()
+
+#files = ['C_high','C_low','C_medium','D_high','D_low','D_medium','In_low','In_high','In_medium',            # List of 18 files
+#         'Ir_high','Ir_low','Ir_medium','Jay_high','Jay_low','Jay_medium','Su_high','Su_low','Su_medium']
+
+files = ['C_high']    				# Files for which granger sausality is to be calculated
+
+for file in files:
+    print(file)
+    data = np.load('data/preprocess/' +file+ '.npy')    # Replace with Loctaion of file on drive
+    granger = []
+    for i in range(1): 
+        print(i,end=' ')
+        arr = np.zeros(shape=(3136))
+        cnt = 0
+        for j in range(5):
+            #print(i,j, end=' ')
+            for k in range(56):
+                arr[cnt] = granger_cause( data[i][j], data[i][k] )
+                cnt += 1
+        granger.append(arr)
+    np.save('data/parameter/'+file+'_granger.npy',granger, allow_pickle=True)   # CHange to location where the file is to be save
+    print(np.load('data/parameter/'+file+'_granger.npy'))
